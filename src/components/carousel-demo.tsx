e0ffe4d6c86b27ad2b0b114006a784615a66b0e3
facuperezm <facundoperezmontalvo@gmail.com>
2024-03-08 15:44:37 -0300

    feat: eslint, prettier and editorconfig

--- src/components/carousel-demo.tsx
+++ src/components/carousel-demo.tsx
@@ -1,35 +1,28 @@
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel"
 
 export default function CarouselDemo({ photos }: { photos: string[] }) {
-  return (
-    <Carousel>
-      <CarouselPrevious
-        variant="ghost"
-        className="absolute top-1/2 left-2 z-10"
-        size="icon"
-      />
-      <CarouselContent>
-        {photos.map((photo, index) => (
-          <CarouselItem key={index}>
-            <img
-              src={photo}
-              alt="property images"
-              className="size-full object-cover aspect-square"
-            />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselNext
-        variant="ghost"
-        className="absolute top-1/2 right-2 z-10"
-        size="icon"
-      />
-    </Carousel>
-  );
+	return (
+		<Carousel>
+			<CarouselPrevious variant="ghost" className="absolute left-2 top-1/2 z-10" size="icon" />
+			<CarouselContent>
+				{photos.map((photo, index) => (
+					<CarouselItem key={index}>
+						<img
+							src={photo}
+							alt="property images"
+							className="aspect-square size-full object-cover"
+						/>
+					</CarouselItem>
+				))}
+			</CarouselContent>
+
+			<CarouselNext variant="ghost" className="absolute right-2 top-1/2 z-10" size="icon" />
+		</Carousel>
+	)
 }
