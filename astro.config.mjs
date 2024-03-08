1fac78298d15eb9e0234ae893558930ea6665204
facuperezm <facundoperezmontalvo@gmail.com>
2024-03-08 16:23:38 -0300

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,30 @@
-import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
-
+import vercel from "@astrojs/vercel/serverless"
 import react from "@astrojs/react"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
 	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
+		tailwind(),
 		react({
 			include: ["**/react/*"],
 			experimentalReactChildren: true,
 		}),
 	],
+	adapter: vercel({
+		webAnalytics: { enabled: true },
+	}),
+	build: {
+		inlineStylesheets: "always",
+	},
+	output: "hybrid",
 	vite: {
 		build: {
 			cssMinify: "lightningcss",
+		},
+		ssr: {
+			noExternal: ["path-to-regexp"],
 		},
 	},
 })
